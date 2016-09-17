efc82db17a068da286d3547527660d0644b70479
DarkAce65 <tvdood99@gmail.com>
2016-09-16 23:15:01 -0400

    Add some filler content and scroll event

--- javascripts/script.js
+++ javascripts/script.js
@@ -26,10 +26,40 @@ function createCard(item) {
 	card += '</div>';
 
 	$("#sectionContent").append(card);
+	return $("#" + item.name);
 }
+
+function addFillerContent(n) {
+	var img = ["IMG_3444 e.jpg", "IMG_3450 e.jpg", "IMG_3452 e.jpg", "IMG_3454 e.jpg", "IMG_3457 e.jpg", "IMG_3461 e.jpg", "IMG_3464 e.jpg", "IMG_3465 e.jpg", "IMG_3466 e.jpg", "IMG_3467 e.jpg", "IMG_3468 e.jpg", "IMG_3469 e.jpg", "IMG_3471 e.jpg", "IMG_3486 e.jpg", "IMG_3481 e.jpg", "IMG_3474 e.jpg"];
+	for(var i = 0; i < n; i++) {
+		var item = {name: "Test" + i, title: "Test " + i, subtitle: "sub"};
+		if(Math.random() < 0.5) {
+			item.galleryItems = img.slice(0, ~~(Math.random() * img.length));
+		}
+		else {
+			item.content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+		}
+		gallery.push(item);
+	}
+}
+
+var unloadedIndices = [];
+addFillerContent(30);
 
 for(var i = 0; i < gallery.length; i++) {
-	$("#menu").append('<span onclick="scrollToContent(\'' + gallery[i].name + '\')">' + gallery[i].name + '</span>');
-
-	createCard(gallery[i]);
+	$("#menu").append('<span onclick="scrollToContent(\'' + gallery[i].name + '\')">' + gallery[i].title + '</span>');
+	unloadedIndices.push(i);
 }
+
+for(var i = 3; i < 6; i++) {
+	createCard(i);
+}
+
+$(document).scroll(function() {
+	if(unloadedIndices.length === 1) {
+		$(document).off("scroll");
+	}
+	if($(document).height() - $(window).scrollTop() - $(window).height() < 150) {
+		createCard(unloadedIndices[0]);
+	}
+});
