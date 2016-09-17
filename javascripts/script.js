a5d6032a25b8b74c0f246b85c9ba133092823fda
DarkAce65 <tvdood99@gmail.com>
2016-09-16 22:27:59 -0400

    Add dynamic image grid sizes

--- javascripts/script.js
+++ javascripts/script.js
@@ -14,14 +14,14 @@ function initContent() {
 function createCard(item) {
 	var card = '<div id="' + item.title + '" class="card"><div class="title"><h2>' + item.title + '</h2><p class="subtitle">' + item.subtitle + '</p></div>';
 	if(item.hasOwnProperty("galleryItems") && item.galleryItems.length > 0) {
-		card += '<hr><div class="gallery">';
+		card += '<div class="gallery square' + Math.round(Math.sqrt(item.galleryItems.length, 2)) + '">';
 		for(var i = 0; i < item.galleryItems.length; i++) {
 			card += '<a href="' + item.galleryItems[i] + '" data-lightbox="' + item.name + '"><img src="' + item.galleryItems[i] + '"></a>';
 		}
 		card += '</div>';
 	}
 	else if(item.hasOwnProperty("content")) {
-		card += '<hr><p class="text">' + item.content + '</p>';
+		card += '<p class="text">' + item.content + '</p>';
 	}
 	card += '</div>';
 
